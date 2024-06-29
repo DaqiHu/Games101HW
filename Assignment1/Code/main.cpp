9559b32082192b8b17047e750aec68c80deaba0f
DaqiHu <hudaqiwork@outlook.com>
2024-06-29 22:51:10 +0800

    format code

--- Assignment1/Code/main.cpp
+++ Assignment1/Code/main.cpp
@@ -1,10 +1,12 @@
-#include "Triangle.hpp"
-#include "rasterizer.hpp"
 #include <Eigen/Dense>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <vector>
 #include <string>
+#include <thread>
+#include <vector>
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+
 
 constexpr double MY_PI = 3.1415926;
 
@@ -13,8 +15,14 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
+
+    // clang-format off
+    translate << 
+        1, 0, 0, -eye_pos[0],
+        0, 1, 0, -eye_pos[1], 
+        0, 0, 1, -eye_pos[2], 
+        0, 0, 0, 1;
+    // clang-format on
 
     view = translate * view;
 
@@ -32,8 +40,7 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     return model;
 }
 
-Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
-                                      float zNear, float zFar)
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
 {
     // Students will implement this function
 
@@ -46,16 +53,34 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     return projection;
 }
 
+auto get_image(float angle, const Eigen::Vector3f& eye_pos, const rst::pos_buf_id& pos_id, const rst::ind_buf_id& ind_id)
+{
+    rst::rasterizer r{700, 700};
+    r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+    r.set_model(get_model_matrix(angle));
+    r.set_view(get_view_matrix(eye_pos));
+    r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+    r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+    cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+    image.convertTo(image, CV_8UC3, 1.0f);
+
+    return image;
+}
+
 int main(int argc, const char** argv)
 {
-    float angle = 0;
-    bool command_line = false;
+    float angle          = 0;
+    bool command_line    = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
+    if (argc >= 3)
+    {
         command_line = true;
-        angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
+        angle        = std::stof(argv[2]); // -r by default
+        if (argc == 4)
+        {
             filename = std::string(argv[3]);
         }
         else
@@ -73,45 +98,29 @@ int main(int argc, const char** argv)
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
 
-    int key = 0;
+    int key         = 0;
     int frame_count = 0;
 
-    if (command_line) {
-        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-
-        r.set_model(get_model_matrix(angle));
-        r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
-
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
-        image.convertTo(image, CV_8UC3, 1.0f);
-
-        cv::imwrite(filename, image);
+    if (command_line)
+    {
+        cv::imwrite(filename, get_image(angle, eye_pos, pos_id, ind_id));
 
         return 0;
     }
 
-    while (key != 27) {
-        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
-
-        r.set_model(get_model_matrix(angle));
-        r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
-
-        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
-
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
-        image.convertTo(image, CV_8UC3, 1.0f);
-        cv::imshow("image", image);
+    while (key != 27)
+    {
+        cv::imshow("image", get_image(angle, eye_pos, pos_id, ind_id));
         key = cv::waitKey(10);
 
         std::cout << "frame count: " << frame_count++ << '\n';
 
-        if (key == 'a') {
+        if (key == 'a')
+        {
             angle += 10;
         }
-        else if (key == 'd') {
+        else if (key == 'd')
+        {
             angle -= 10;
         }
     }
